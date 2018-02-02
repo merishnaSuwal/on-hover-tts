513a4fa151fd6f1f16d34c04fff69c50f2122af2
Pragyan Subedi <pragyansubedi@gmail.com>
2018-02-02 11:01:11 +0545

    Added speaking text objects

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-$(document).mousemove(function(e){
+var tagList = ['HTML', 'HEAD', 'BODY', 'DIV', 'SECTION']; 
 
+$(document).mousemove(function(e){
+	var target = $(e.target);
+    var inputtext = target.val();
+    var inputmsg = new SpeechSynthesisUtterance(inputtext);
+    var msgtext = target.text();
+    var msg = new SpeechSynthesisUtterance(msgtext);
+    var msgalt = target.attr("alt");
+    var msgaltnew = new SpeechSynthesisUtterance(msgalt);
+    var msglabel= target.attr('aria-label');
+    var msglabelnew = new SpeechSynthesisUtterance(msglabel);
+
+    if(tagList.indexOf(target.prop("tagName")) == -1){
+    	
 
 });
