4e8d3531a04004d8c5c614347e6b2c71add9b2cb
Pragyan Subedi <pragyansubedi@gmail.com>
2018-02-02 12:08:01 +0545

    script working

--- script.js
+++ script.js
@@ -2,22 +2,23 @@ var tagList = ['HTML', 'HEAD', 'BODY', 'DIV', 'SECTION'];
 
 $(document).mousemove(function(e){
 	var target = $(e.target);
-    var inputtext = target.value();
+    var inputtext = target.val();
     var inputmsg = new SpeechSynthesisUtterance(inputtext);
     var msgtext = target.text();
     var msg = new SpeechSynthesisUtterance(msgtext);
     var msgalt = target.attr("alt");
     var msgaltnew = new SpeechSynthesisUtterance(msgalt);
-    var msglabel= target.attr('aria-label');
-    var msglabelnew = new SpeechSynthesisUtterance(msglabel);
+   // var msglabel= target.attr('aria-label');
+   // var msglabelnew = new SpeechSynthesisUtterance(msglabel);
 
     function speaker(){
 		speechSynthesis.speak(msg);
         speechSynthesis.speak(inputmsg);
         speechSynthesis.speak(msgaltnew);
-        speechSynthesis.speak(msglabelnew);
+       // speechSynthesis.speak(msglabelnew);
 		}
     function stopSpeaker(){
+    	target.removeClass("speakText");
     	speechSynthesis.cancel(); 
     }
 
@@ -39,9 +40,11 @@ $(document).mousemove(function(e){
 
     if(tagList.indexOf(target.prop("tagName")) == -1){
     	target.addClass("speakText");
-		$(target).mouseleave(function(){
-            stopSpeaker();  
-        });
+    	setTimeout(function(){
+		    $(target).mouseleave(function(){
+		        stopSpeaker();  
+		    });
+   		},100);
 		classCheck();
     }	 	
 });
