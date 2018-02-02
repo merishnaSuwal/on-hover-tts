95471e67865178bb1c0b5d2864c54d44504cba13
Pragyan Subedi <pragyansubedi@gmail.com>
2018-02-02 13:19:29 +0545

    Added options page

--- script.js
+++ script.js
@@ -1,50 +1,73 @@
 var tagList = ['HTML', 'HEAD', 'BODY', 'DIV', 'SECTION']; 
-
-$(document).mousemove(function(e){
-	var target = $(e.target);
-    var inputtext = target.val();
-    var inputmsg = new SpeechSynthesisUtterance(inputtext);
-    var msgtext = target.text();
-    var msg = new SpeechSynthesisUtterance(msgtext);
-    var msgalt = target.attr("alt");
-    var msgaltnew = new SpeechSynthesisUtterance(msgalt);
-   // var msglabel= target.attr('aria-label');
-   // var msglabelnew = new SpeechSynthesisUtterance(msglabel);
-
-    function speaker(){
-		speechSynthesis.speak(msg);
-        speechSynthesis.speak(inputmsg);
-        speechSynthesis.speak(msgaltnew);
-       // speechSynthesis.speak(msglabelnew);
-		}
-    function stopSpeaker(){
-    	target.removeClass("speakText");
-    	speechSynthesis.cancel(); 
-    }
-
-	function classCheck()
-    {
-        if(target.is(".speakText")) {
-            speaker();
-            var isSpeaking=true;
-            // USE CTRL TO STOP
-            if(isSpeaking) {
-                $(document).keyup(function(e) {
-                    if(e.key === "Control"){
-                    stopSpeaker();
+$(document).ready(function(){ 
+    chrome.storage.sync.get({
+        setState: 'enable',
+        setRate: '1',
+        }, function(items) {
+            if (items.setState === "enable")
+            {
+            $(document).keydown(function(e){
+                if(e.key == 'F11')
+                {
+                    var fullscreentext = "Full screen on";
+                    var fullscreenmsg = new SpeechSynthesisUtterance(fullscreentext);
+                    fullscreenmsg.rate=items.setRate;
+                    speechSynthesis.speak(fullscreenmsg);   
                 }
-                });
-            } 
-        }
-    }
+            });
+            // MAIN PROGRAM
+            $(document).mousemove(function (e) {
+                var target = $(e.target);
+                var inputtext = target.val();
+                var inputmsg = new SpeechSynthesisUtterance(inputtext);
+                var msgtext = target.text();
+                var msg = new SpeechSynthesisUtterance(msgtext);
+                var msgalt = target.attr("alt");
+                var msgaltnew = new SpeechSynthesisUtterance(msgalt);
+                var msglabel= target.attr('aria-label');
+                var msglabelnew = new SpeechSynthesisUtterance(msglabel);
 
-    if(tagList.indexOf(target.prop("tagName")) == -1){
-    	target.addClass("speakText");
-    	setTimeout(function(){
-		    $(target).mouseleave(function(){
-		        stopSpeaker();  
-		    });
-   		},100);
-		classCheck();
-    }	 	
+                //TO SPEAK
+                function speaker()
+                {   
+                    //$(target).css("font-size", "40px");
+                    speechSynthesis.speak(msg);
+                    speechSynthesis.speak(inputmsg);
+                    speechSynthesis.speak(msgaltnew);
+                    speechSynthesis.speak(msglabelnew);
+                }
+                //TO STOP
+                function stopSpeaker()
+                {
+                    target.removeClass("speakText");
+                    speechSynthesis.cancel();
+                }
+                //TO CHECK CLASS
+                function classCheck()
+                {
+                    if(target.is(".speakText") ) {
+                        speaker();
+                        var isSpeaking=true;
+                        // USE CTRL TO STOP
+                        if(isSpeaking) {
+                            $(document).keyup(function(e) {
+                                if(e.key === "Control"){
+                                stopSpeaker();
+                            }
+                            });
+                        } 
+                    }
+                }
+                if(tagList.indexOf(target.prop("tagName")) == -1){
+                        target.addClass("speakText");
+                       setTimeout(function(){
+                            $(target).mouseleave(function(){
+                                stopSpeaker();  
+                            });
+                       },100);
+                        classCheck();     
+                    }
+            }); //END of MAIN PROGRAM
+        }    
+    });
 });
