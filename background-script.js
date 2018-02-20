030ab8c2dd01f1ce3a85b50e18ef3dcfb8836feb
merishnaSuwal <merishna97@gmail.com>
2018-02-20 14:15:02 +0545

    Added rate control

--- background-script.js
+++ background-script.js
@@ -1,22 +1,18 @@
 
 chrome.runtime.onMessage.addListener(function(request) {
-   chrome.tts.speak(request.toSay, 
+      chrome.tts.speak(request.toSay, 
       {
-       rate: 1 
+       rate: parseInt(request.speakRate)
       });
-
-   if(request.pauser==="YES")
-    {
+      if (request.stop === "YES"){
+        chrome.tts.stop();
+      }
+      if (request.pauser === "YES"){
         chrome.tts.pause();
-    }
-
-    if (request.stop === "YES"){
-      chrome.tts.stop();
-    }
-    if(request.resumer === "YES")
-    {
-       chrome.tts.resume();
-    }
+      }
+       if (request.resumer === "YES"){
+        chrome.tts.resume();
+      }
     
 });
 
--- script.js
+++ script.js
@@ -9,121 +9,120 @@ $(document).ready(function(){
         }, function(items) {
             if (items.setState === "enable")
             {
-            
-	    // START OF
-            // MAIN PROGRAM
-            function ttsSpeaker(x){
-                chrome.runtime.sendMessage({toSay: x, stop: "NO", resumer: "NO", pauser: "NO"}, function() {});
-            }
-            function ttsStop(x){
-                chrome.runtime.sendMessage({toSay: x, stop: "YES", resumer: "NO", pauser: "NO"}, function() {});
-            }
-            function ttsPause(x){
-                chrome.runtime.sendMessage({toSay: x, stop: "NO", resumer: "NO", pauser: "YES"}, function() {});
-            }
-            function ttsResume(x){
-                chrome.runtime.sendMessage({toSay: x, stop: "NO", resumer: "YES", pauser: "NO"}, function() {});
-            }
-            $(document).keydown(function(e){
-                if(e.key == 'F11')
-                {
-                    var fullscreentext = "Full screen on";
-                    ttsSpeaker(fullscreentext);
+               
+    	        // START OF
+                // MAIN PROGRAM
+                function ttsSpeaker(x,y){
+                    chrome.runtime.sendMessage({speakRate: y, toSay: x, stop: "NO", resumer: "NO", pauser: "NO"}, function() {});
                 }
-            });
+                function ttsStop(x,y){
+                    chrome.runtime.sendMessage({speakRate: y, toSay: x,stop: "YES",resumer: "NO", pauser: "NO"}, function() {});
+                }
+                function ttsPause(x,y){
+                    chrome.runtime.sendMessage({speakRate: y, toSay: x, stop: "NO",resumer: "NO", pauser: "YES"}, function() {});
+                }
+                function ttsResume(x,y){
+                    chrome.runtime.sendMessage({speakRate: y,toSay: x, stop: "NO",resumer: "YES", pauser: "YES"}, function() {});
+                }
+                $(document).keydown(function(e){
+                    if(e.key == 'F11')
+                    {
+                        var fullscreentext = "Full screen on";
+                        ttsSpeaker(fullscreentext);
+                    }
+                });
 
-            $(document).mouseover(function (e) {
-                //SELECT TARGET
+                $(document).mouseover(function (e) {
+                    //SELECT TARGET
+                    var target = $(e.target);                
 
-                var target = $(e.target);                
+                    //FOR INPUT FIELD
+                    var inputtext = target.attr("placeholder");
+                    if(target.prop("tagName")=="INPUT"){
+                     inputtext = "Input field" + inputtext;
+                    }
+                    //FOR BUTTON TEXT
+                    var msgtext = target.text();
+                    if(target.prop("tagName")=="BUTTON"){
+                       msgtext = "Button text" + msgtext;
+                    }
+                    if(target.prop("tagName")=="A"){
+                       msgtext = "Link text" + msgtext;
+                    }
+                    //FOR ALT TEXT
+                    var msgalt = target.attr("alt");
 
-                //FOR INPUT FIELD
-                var inputtext = target.attr("placeholder");
-                if(target.prop("tagName")=="INPUT"){
-                 inputtext = "Input field" + inputtext;
-                }
-                //FOR BUTTON TEXT
-                var msgtext = target.text();
-                if(target.prop("tagName")=="BUTTON"){
-                   msgtext = "Button text" + msgtext;
-                }
-                if(target.prop("tagName")=="A"){
-                   msgtext = "Link text" + msgtext;
-                }
-                //FOR ALT TEXT
-                var msgalt = target.attr("alt");
+                    //FOR ARIA LABELS
+                    var msglabel= target.attr('aria-label');
 
-                //FOR ARIA LABELS
-                var msglabel= target.attr('aria-label');
+                    //TO SPEAK
+                    function speaker()
+                    {   
+                        ttsSpeaker(msgtext,items.setRate);
+                        ttsSpeaker(msgalt,items.setRate);
+                        ttsSpeaker(msglabel,items.setRate);
+                        ttsSpeaker(inputtext,items.setRate);
+                    }
 
-                //TO SPEAK
-                function speaker()
-                {   
-                    ttsSpeaker(msgtext);
-                    ttsSpeaker(msgalt);
-                    ttsSpeaker(msglabel);
-                    ttsSpeaker(inputtext);
-                }
+                    function pauseSpeaker(){
+                        target.removeClass("speakText");
+                        ttsPause(msgtext,items.setRate);
+                        ttsPause(msgalt,items.setRate);
+                        ttsPause(msglabel,items.setRate);
+                        ttsPause(inputtext,items.setRate);
+                    }
+                    function resumeSpeaker(){
+                        target.addClass("speakText");
+                        ttsResume(msgtext,items.setRate);
+                        ttsResume(msgalt,items.setRate);
+                        ttsResume(msglabel,items.setRate);
+                        ttsResume(inputtext,items.setRate);
 
-                function pauseSpeaker(){
-                    target.removeClass("speakText");
-                    ttsPause(msgtext);
-                    ttsPause(msgalt);
-                    ttsPause(msglabel);
-                    ttsPause(inputtext);
-                }
-                function resumeSpeaker(){
-                    target.addClass("speakText");
-                    ttsResume(msgtext);
-                    ttsResume(msgalt);
-                    ttsResume(msglabel);
-                    ttsResume(inputtext);
-
-                }
-                //TO STOP
-                function stopSpeaker()
-                {
-                    target.removeClass("speakText");
-                    ttsStop("STOP");
-                }
-                //TO CHECK CLASS
-                function classCheck()
-                {
-                    if(target.is(".speakText") ) {
-                        speaker();
-                        var isSpeaking=true;
-                        // USE CTRL TO STOP
-                        if(isSpeaking) {
-                            $(document).keyup(function(e) {
-                                if(e.key === "Control"){
-                                    pauseSpeaker();
-                                    var isSpeaking = false;
-                                }
+                    }
+                    //TO STOP
+                    function stopSpeaker()
+                    {
+                        target.removeClass("speakText");
+                        ttsStop();
+                    }
+                    //TO CHECK CLASS
+                    function classCheck()
+                    {
+                        if(target.is(".speakText") ) {
+                            speaker();
+                            var isSpeaking=true;
+                            // USE CTRL TO STOP
+                            if(isSpeaking) {
                                 $(document).keyup(function(e) {
-                                    if(!isSpeaking){
-                                    
-                                        if(e.key === "Shift"){
-                                            resumeSpeaker();
-                                            var isSpeaking = true;
-                                        }
+                                    if(e.key === "Control"){
+                                        pauseSpeaker();
+                                        var isSpeaking = false;
                                     }
-                                });
-                           });
+                                    $(document).keyup(function(e) {
+                                        if(!isSpeaking){
+                                        
+                                            if(e.key === "Shift"){
+                                                resumeSpeaker();
+                                                var isSpeaking = true;
+                                            }
+                                        }
+                                    });
+                               });
+                            }
+                            
                         }
-                        
                     }
-                }
-                if(tagList.indexOf(target.prop("tagName")) == -1){
-                    target.addClass("speakText");
-                    setTimeout(function(){
-                        $(target).mouseleave(function(){
-                            stopSpeaker();  
-                        });
-                    },10);
-                    classCheck();   
-                    }
-            }); //END of MAIN PROGRAM
-        }    
+                    if(tagList.indexOf(target.prop("tagName")) == -1){
+                        target.addClass("speakText");
+                        setTimeout(function(){
+                            $(target).mouseleave(function(){
+                                stopSpeaker();  
+                            });
+                        },10);
+                        classCheck();   
+                        }
+
+                 }); //END of MAIN PROGRAM
+            }     
     });
 });
-
